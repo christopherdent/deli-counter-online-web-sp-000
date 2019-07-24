dec7e144eae2cc561654533b843cf527aae2351a
Christopher Dent <christopherdent01@gmail.com>
2019-07-24 17:33:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -38,7 +38,7 @@ def now_serving(array)
     puts "There is nobody waiting to be served!"
   else
     
-    puts "Currently serving #{array[1]}"
+    puts "Currently serving #{array[0]}"
     array.pop
     
 end 
