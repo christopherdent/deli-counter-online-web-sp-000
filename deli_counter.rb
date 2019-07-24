475d5c3d744e1c44e723c66b75dcc591ec75f677
Christopher Dent <christopherdent01@gmail.com>
2019-07-24 16:36:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,42 @@
-# Write your code here.
+
+
+def line(deli)
+
+  if deli.empty?
+   puts "The line is currently empty."
+
+  else 
+
+    current_line = "The line is currently:"
+    
+      deli.each.with_index(1) do |person, i|
+        current_line << " #{i}. #{person}"
+      end
+      
+    puts current_line
+  
+  end
+ 
+end  
+
+
+#########################################################################################################
+
+
+def take_a_number(array, name)
+    if array.empty?
+    array.push(name)
+    puts "Welcome, #{name}. You are number 1 in line."
+    
+  else 
+    array.push(name)
+    
+    array.each.with_index(1) do |name, index|
+      
+      puts "Welcome, #{name}. You are number #{index} in line."
+      
+    end
+    
+  end
+
+end 
