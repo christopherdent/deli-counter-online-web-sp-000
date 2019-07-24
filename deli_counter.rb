e2b59d69ce6019bef9b2996d785fe564b653762a
Christopher Dent <christopherdent01@gmail.com>
2019-07-24 16:47:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -31,7 +31,7 @@ def take_a_number(array, name)
   else 
     array.push(name)
     
-    array.index do |x| 
+    array.index(name) do |x| 
     
     puts "Welcome, #{name}. You are number #{x} in line."
       
