cb624f44834302122444946f7ade6207478abee1
Christopher Dent <christopherdent01@gmail.com>
2019-07-24 17:03:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,11 +25,8 @@ end
 
 def take_a_number(array, name)
    array.push(name)
-   
-   array.each.with_index(1) do |name, index|
-    
-   puts "Welcome, #{name}. You are number #{index} in line."
+        
+   puts "Welcome, #{name}. You are number #{array.count} in line."
       
    end 
   
-end 
